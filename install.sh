10ef7f9dac08390c7c7fa2ca7c5f42320008b16d
Eugene Chou <euchou@ucsc.edu>
2023-05-08 01:22:15 +0200

    use `XDG_CONFIG_HOME` like with everything else

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 # Relink configuration.
-rm -f "$HOME/.tmux.conf"
-rm -f "$HOME/.tmux.conf.local"
-ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$PWD/.tmux.conf.local" "$HOME/.tmux.conf.local"
+rm -rf "$HOME/.config/tmux"
+ln -sf "$PWD" "$HOME/.config/tmux"
